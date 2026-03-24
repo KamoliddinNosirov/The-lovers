10629db9f98884d38cb0da432e562a36a402efa0
KamoliddinNosirov <kamoliddin3425@gmail.com>
2026-03-24 23:12:04 +0500

    3 add

--- js/scritp.js
+++ js/scritp.js
@@ -1,8 +1,15 @@
 const langageAll = document.querySelectorAll(".til")
+const like = document.querySelector(".like")
+const line = document.querySelector(".line")
 
 langageAll.forEach((langage)=>{
     langage.addEventListener("click", ()=>{
         langageAll.forEach(el => el.classList.remove("active"))
         langage.classList.add("active")
     })
+})
+
+like.addEventListener("click", ()=>{
+    like.classList.toggle("active")
+    line.classList.toggle("active")
 })
