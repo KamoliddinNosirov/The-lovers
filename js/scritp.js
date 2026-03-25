f61a0588437bf4560108d216ccce5d8475315d5e
KamoliddinNosirov <kamoliddin3425@gmail.com>
2026-03-25 21:27:52 +0500

    4 add, loyiha borgan sari go'zallashib bormoqda

--- js/scritp.js
+++ js/scritp.js
@@ -1,15 +1,30 @@
 const langageAll = document.querySelectorAll(".til")
 const like = document.querySelector(".like")
 const line = document.querySelector(".line")
+const lock = document.querySelector(".lock")
+const modal = document.querySelector(".modal")
+const hero = document.querySelector(".hero")
+const h1Tag = document.querySelectorAll(".user_name")
 
-langageAll.forEach((langage)=>{
-    langage.addEventListener("click", ()=>{
+langageAll.forEach((langage) => {
+    langage.addEventListener("click", () => {
         langageAll.forEach(el => el.classList.remove("active"))
         langage.classList.add("active")
     })
 })
 
-like.addEventListener("click", ()=>{
+like.addEventListener("click", () => {
     like.classList.toggle("active")
     line.classList.toggle("active")
+})
+
+lock.addEventListener("click", () => {
+    modal.classList.add("active")
+    setTimeout(() => {
+        hero.classList.add("remove")
+    }, 500);
+    setTimeout(() => {
+        modal.classList.add("remove")
+        h1Tag.forEach((item)=> item.classList.add("active"))
+    }, 1000);
 })
